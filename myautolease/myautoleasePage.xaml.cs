6451c393e0b8430b829e93c39743204e8f9f8bbb
PHIL H KIM <philhkim@gmail.com>
2017-07-26 00:16:42 -0400

    new project

---
+++ myautolease/myautoleasePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace myautolease
+{
+    public partial class myautoleasePage : ContentPage
+    {
+        public myautoleasePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
